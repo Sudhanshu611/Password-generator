41b0dbfd8c6852dc201cad50afd70c65c9b9d37b
Sudhanshu Singh <sudhanshu0300@gmail.com>
2024-09-16 12:17:17 +0530

    Here is your code

--- Generator.py
+++ Generator.py
@@ -1,16 +1,33 @@
 import random
 import string
 
-len_pass = int(input("How long your password must be? => "))
-num_pass = int(input("How many passwords you want? => "))
-password = ""
-def generator():
-    global password
-    char = string.punctuation + string.ascii_uppercase + string.ascii_lowercase + string.digits
-    for i in range(len_pass):
-      password += random.choice(char)
+generator = True
 
-for x in range(num_pass):
-    generator()
-    print(password)
-    password = ""
+while generator:
+  len_pass = int(input("How long your password must be? => "))
+  num_pass = int(input("How many passwords you want? => "))
+  password = ""
+  def generator():
+      global password
+      char = string.punctuation + string.ascii_uppercase + string.ascii_lowercase + string.digits
+      for i in range(len_pass):
+        password += random.choice(char)
+
+  for x in range(num_pass):
+      generator()
+      print(password)
+      password = ""
+      
+  while True:
+    ask = input("You Want More? (y/n)\n>")
+    
+    if not ask.isalpha():
+      print("Write Number Only")
+    elif ask.lower() == 'y':
+      print("Here You Go!!")
+      break
+    elif ask.lower() == 'n':
+      print("Fine!! Have a great security.")
+      generator = False
+      break
+      
